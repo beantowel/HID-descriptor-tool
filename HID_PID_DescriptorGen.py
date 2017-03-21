ba06601f0d607178d6b3b1fa813ab2607b37d854
beantowel <chiefbean@foxmail.com>
2017-03-21 19:51:45 +0800

    initial commit

---
+++ HID_PID_DescriptorGen.py
@@ -0,0 +1,106 @@
+import re
+from HID_PID_Definitions import *
+def Byte_Size(x):
+	'''ByteSize for Short Items
+	return 0,1,2,or4'''
+	x=int(x)
+	if x>=-0x7F-1 and x<=0x7F:
+		return 1
+	if x>=-0x7FFF-1 and x<=0x7FFF:
+		return 2
+	if x>=-0x7FFFFFFF-1 and x<=0x7FFFFFFF:
+		return 4
+def u_Byte_Size(x):
+	'''ByteSize for Short Items
+	return 0,1,2,or4'''
+	x=int(x)
+	if x<=0xFF:
+		return 1
+	if x<=0xFFFF:
+		return 2
+	if x<=0xFFFFFFFF:
+		return 4
+def toComplementBytes(x,size):
+	x=int(x)
+	if x<0:
+		x=x+2**size
+	return hex(x)
+def prefixZero(hdata,byteSize):
+	'''fill prefixZero to complete a byte'''
+	#input like '0xNNN'
+	lsize=len(hdata)-2
+	while(lsize!=byteSize*2): #0xN-->0x0N
+		hdata="0x0"+hdata[2:]
+		lsize=len(hdata)-2
+	return hdata
+def SplitToLittleEndien(hdata,byteSize):
+	datalist=[]
+	hdata=prefixZero(hdata,byteSize)
+	for i in range(0,byteSize*2,2): #0x1234
+		datalist.insert(0,"0x"+hdata[2+i:4+i]) #[0x34,0x12]
+	return datalist
+def ShortItem(prefix,x,isSign):
+	'''construct short item by prefix'''
+	global bytecount
+	if isSign:
+		byteSize=Byte_Size(x)
+	else:
+		byteSize=u_Byte_Size(x)
+	pre=hex(prefix+byteSize)
+	pre=prefixZero(pre,1)
+
+	size=byteSize*8
+	if prefix==0x54: #Unit_Exponent exception
+		size=4 #one nibble
+	hdata=toComplementBytes(x,size)
+	data=SplitToLittleEndien(hdata,byteSize)
+	if prefix==0xc0: #End_Collection exception
+		bytecount+=1
+		return "0xc0",[]
+	bytecount=bytecount+1+len(data)
+	return pre,data
+fileIn="HID_Descriptor_Input.rptDsc"
+fileOut=open("HID_Descriptor.out",'w')
+lines=open(fileIn).readlines()
+bytecount=0
+#descriptor parser
+for line in lines:
+	line=line.expandtabs(tabsize=4)
+	copyline=line
+	line=line.lstrip()
+	pos=line.find('//')
+	if pos>=0:
+		line=line[0:pos]
+
+	b=False
+	for items in HID_Items:
+		for regex in items:
+			item=re.findall('\\b'+regex+'\\b',line)
+			for i in item:
+				prefix=items[regex]
+				value=re.findall('\(.*\)',line)
+				for consts in HID_Constants:
+					for regex1 in consts:
+						const=re.findall('\('+regex1+'\)',value[0])
+						for j in const:
+							x=consts[regex1]
+							out=ShortItem(prefix,x,False)
+							b=True
+				if not(b) and len(value[0])>2:
+					x=int(value[0][1:-1])
+					out=ShortItem(prefix,x,True)
+					b=True
+				if not(b):
+					out=ShortItem(prefix,0,True) #defalut x=0
+					b=True
+	if not(b):
+		continue
+	#print(out)
+	fileOut.write(out[0]+',')
+	for i in out[1]:
+		fileOut.write(i+',')
+	for i in range(0,4-len(out[1])): #max 4 byte
+		fileOut.write('     ')
+	fileOut.write(" //"+copyline) #copy source as comments
+fileOut.write("//Total:"+str(bytecount)+" Bytes")
+fileOut.close()
---
+++ Readme.md
@@ -0,0 +1,20 @@
+# HID-PID Descriptor tool
+an extendable hid descriptor tool,parses the report descriptor written by natural language.  
+
+## Files  
+- \rptDsc:  
+YAML syntax definition for Sublime Text 3 to highlight the report descriptor written in natural language. In Windows copy this to "C:\Users\Administrator\AppData\Roaming\Sublime Text 3\Packages\User", then choose syntax "rptDsc" when editing with Sublime Text 3.  
+- HID\_PID\_Descriptor\_Definitions.py:  
+Human Interface Device & Physical Interface Device usage definitions.  
+- HID\_PID\_DescriptorGenTool.py:  
+parses the .rptDsc into bytes(report descriptor).  
+- HID\_PID\_DataStructureGen.py:  
+parses the .rptDsc into c language data structure definitions.  
+  
+## How to Use  
+- Before writing report descriptor in HID\_Descriptor\_Input.rptDsc, you are recommended to copy \rptDsc to the path refered above so that syntax highlight can work. To parse the descriptor into bytes run DescriptorGenTool, to parse into c language data structure run DataStructureGen.py.   
+  
+## Parser Limitations  
+For the purpose of freeing developers from the tedious and error prone works of writing the Report Descriptor or the data structure attached to it, i made this parser. However, the USB HID Report Descriptor itself was rather a flexible protocol. There is no perfect approach to an easy solution of all the problems we meet in developing.    
+### bugs been found:  
+- c doesn't support syntax like "enum XXX : _type {...}" to designate specific type to the enum.
